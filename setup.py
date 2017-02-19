b539020a4320cc71b1f131bd083a13003966e304
Raymond Schmidt <raymond.schmidt@googlemail.com>
2017-02-19 14:39:18 +0100

    next

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='franz_mosaic',
     packages=['franz_mosaic'],
-    version='v1.02',  # Ideally should be same as your GitHub release tag varsion
+    version='v1.03',  # Ideally should be same as your GitHub release tag varsion
     description='reduces pictures and creates new image based on reduced information',
     author='raymond schmidt',
     author_email='raymond.schmidt@googlemail.com',
     url='https://github.com/kingmray/franz_mosaic',
-    download_url='https://github.com/kingmray/franz_mosaic/archive/v1.0.2.tar.gz',
+    download_url='https://github.com/kingmray/franz_mosaic/archive/v1.0.3.tar.gz',
     keywords=['image', 'processing'],
     classifiers=[],
     entry_points={
